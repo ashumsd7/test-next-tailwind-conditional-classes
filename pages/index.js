8609802c6259161801839a6a888031a998c1cdfd
aathym <aat8001@gmail.com>
2022-10-14 17:54:00 +0530

    inti 2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { RhButton } from "@rhythm-ui/react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <RhButton variant="primary">Hello</RhButton>
       <div className="h-20 text-3xl text-green-500 rounded-md lg:text-red-700 md:text-blue-500">
         Test v1 text-green-500 rounded-md lg:text-red-700 md:text-blue-500
       </div>
